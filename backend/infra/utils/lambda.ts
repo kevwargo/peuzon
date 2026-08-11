9107eeb9e8576a32caba18776770e5d4c090bf22
kevwargo <jarasz.kevwargo@gmail.com>
2026-08-11 14:24:19 +0300

    Compatibility improvement

--- backend/infra/utils/lambda.ts
+++ backend/infra/utils/lambda.ts
@@ -35,6 +35,11 @@ function ensureLayer(scope: Construct): LayerVersion {
     return node as LayerVersion;
   }
 
+  let user: string | undefined;
+  if (process.getuid && process.getgid) {
+    user = `${process.getuid()}:${process.getgid()}`;
+  }
+
   return new LayerVersion(stack, layerNodeId, {
     compatibleRuntimes: [runtime],
     code: Code.fromAsset(srcDir, {
@@ -54,7 +59,7 @@ function ensureLayer(scope: Construct): LayerVersion {
             `uv pip install --requirements ${assetReqs} --target /asset-output/python`,
           ].join(" && "),
         ],
-        user: `${process.getuid!()}:${process.getgid!()}`,
+        user,
         environment: {
           UV_CACHE_DIR: uvCacheMnt,
           UV_LINK_MODE: "copy",
