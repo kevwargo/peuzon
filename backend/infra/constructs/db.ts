e8c62933d9539ef3ff6fca9f00a82ab2c6336356
kevwargo <jarasz.kevwargo@gmail.com>
2026-08-12 10:40:30 +0300

    Fun refactor of infra/constructs/db.ts

--- backend/infra/constructs/db.ts
+++ backend/infra/constructs/db.ts
@@ -1,80 +1,55 @@
 import { RemovalPolicy } from "aws-cdk-lib";
-import { AttributeType, BillingMode, Table, TableProps } from "aws-cdk-lib/aws-dynamodb";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
 
 export class Db extends Construct {
   public readonly devices: Table;
-  public readonly locations: Table;
   public readonly sessions: Table;
+  public readonly locations: Table;
   public readonly apiKeys: Table;
 
   constructor(scope: Construct) {
     super(scope, "Db");
 
-    this.devices = this.createTable("Devices", {
-      partitionKey: {
-        name: "id",
-        type: AttributeType.STRING,
-      },
+    this.devices = this.createStringTable("Devices", "id");
+    this.sessions = this.createStringTable("Sessions", "deviceId", {
+      sortKey: "id",
+      indexes: { startTime: ["deviceId", "startTime"] },
     });
-
-    this.locations = this.createTable("Locations", {
-      partitionKey: {
-        name: "deviceId",
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: "timestamp",
-        type: AttributeType.STRING,
-      },
+    this.locations = this.createStringTable("Locations", "deviceId", {
+      sortKey: "timestamp",
+      indexes: { session: ["deviceId", "sessionId"] },
     });
-    this.locations.addGlobalSecondaryIndex({
-      indexName: "session",
-      partitionKey: {
-        name: "deviceId",
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: "sessionId",
-        type: AttributeType.STRING,
-      },
-    });
-
-    this.sessions = this.createTable("Sessions", {
-      partitionKey: {
-        name: "deviceId",
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: "id",
-        type: AttributeType.STRING,
-      },
-    });
-    this.sessions.addGlobalSecondaryIndex({
-      indexName: "startTime",
-      partitionKey: {
-        name: "deviceId",
-        type: AttributeType.STRING,
-      },
-      sortKey: {
-        name: "startTime",
-        type: AttributeType.STRING,
-      },
-    });
-
-    this.apiKeys = this.createTable("ApiKeys", {
-      partitionKey: {
-        name: "hash",
-        type: AttributeType.STRING,
-      },
-    });
+    this.apiKeys = this.createStringTable("ApiKeys", "hash");
   }
 
-  private createTable(name: string, props: TableProps) {
-    return new Table(this, name, {
+  private createStringTable(name: string, partKey: string, props?: StringTableProps) {
+    const table = new Table(this, name, {
       billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: RemovalPolicy.DESTROY,
-      ...props,
+      partitionKey: {
+        name: partKey,
+        type: AttributeType.STRING,
+      },
+      ...(props?.sortKey ? { sortKey: { name: props.sortKey, type: AttributeType.STRING } } : {}),
     });
+
+    Object.entries(props?.indexes ?? {}).forEach(([indexName, keys]) => {
+      table.addGlobalSecondaryIndex({
+        indexName,
+        partitionKey: {
+          name: keys[0],
+          type: AttributeType.STRING,
+        },
+        ...(keys.length > 1 ? { sortKey: { name: keys[1], type: AttributeType.STRING } } : {}),
+      });
+    });
+
+    return table;
   }
+}
+
+interface StringTableProps {
+  sortKey?: string;
+  indexes?: { [key: string]: string[] };
 }
